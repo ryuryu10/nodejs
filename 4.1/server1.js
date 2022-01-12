d9558c276ff9244fd646604cd0cc2ee7f27bb2f1
Seungwoo Yoo <ryuryu10@naver.com>
2022-01-12 17:12:20 +0900

    Create server1.js

---
+++ 4.1/server1.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>Hello Node Server</h1>');
+    res.end('<p>res.end</p>');
+})
+    .listen(8080, () => {
+        console.log('8080 on');
+    });
