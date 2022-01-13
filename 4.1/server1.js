6382835d7ba624ed040876aadc636911d3fa6030
Seungwoo Yoo <ryuryu10@naver.com>
2022-01-13 19:25:50 +0900

    html 연동

--- 4.1/server1.js
+++ 4.1/server1.js
@@ -1,22 +1,20 @@
 const http = require("http");
+const fs = require("fs").promises;
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>Hello Node Server</h1>');
-    res.end('<p>res.end</p>');
+http.createServer(async (req, res) => {
+    try {
+        const data = await fs.readFile('./index.html');
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+        res.end(data);
+    } catch (err) {
+        console.log(err);
+        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8'});
+        res.end(err.message);
+    }
 })
+
     .listen(8080, () => {
-        console.log('8080 on');
-    });
+        console.log("8080 on");
+    })
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write('<h1>Hello Node Server 8081</h1>');
-    res.end('<p>res.end</p>');
-})
-    .listen(8081, () => {
-        console.log('8081 on');
-    });
-
-
-    
+     
