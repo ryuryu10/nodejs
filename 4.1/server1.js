b5ff480cbb05c277bab8e6963c6c81816325f848
Seungwoo Yoo <ryuryu10@naver.com>
2022-01-13 14:41:54 +0900

    다중포트 서버

--- 4.1/server1.js
+++ 4.1/server1.js
@@ -8,3 +8,15 @@ http.createServer((req, res) => {
     .listen(8080, () => {
         console.log('8080 on');
     });
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write('<h1>Hello Node Server 8081</h1>');
+    res.end('<p>res.end</p>');
+})
+    .listen(8081, () => {
+        console.log('8081 on');
+    });
+
+
+    
