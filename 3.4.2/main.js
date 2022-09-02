dd281791c9ef1f648bdcd3fae46766c06ac1167d
Seungwoo Ryu <ryuryu10@naver.com>
2022-09-02 09:33:18 +0900

    3.4.2

---
+++ 3.4.2/main.js
@@ -0,0 +1,15 @@
+// const string = 'Hellooo';
+// const num = 12345;
+// const bool = false;
+// const objects = {
+//     Stage1: {
+//         Stage2: {
+//             THIS_IS_KEY: ' THIS_IS_VALUE',
+//         },
+//     },
+// };
+// console.log(string, num, bool, objects);
+// console.time('for문을 처리하는데 소요된 시간');
+// console.timeEnd('다르게 입력한다면');
+console.error('이것은 요류다');
+console.log('??');
